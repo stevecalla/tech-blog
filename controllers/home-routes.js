23414791a0156cec4bf322342a4fe9d386ba12ee
Steve Calla <callasteven@gmail.com>
2022-09-18 09:48:55 -0600

    Create initial signup login pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -58,34 +58,6 @@ router.get('/gallery/:id', middleware, async (req, res) => {
     res.status(500).json(err);
   }
 
-  // If the user is not logged in, redirect the user to the login page
-  // if (!req.session.loggedIn) {
-  //   res.redirect('/login');
-  // } else {
-  //   // If the user is logged in, allow them to view the gallery
-  //   try {
-  //     const dbGalleryData = await Gallery.findByPk(req.params.id, {
-  //       include: [
-  //         {
-  //           model: Painting,
-  //           attributes: [
-  //             'id',
-  //             'title',
-  //             'artist',
-  //             'exhibition_date',
-  //             'filename',
-  //             'description',
-  //           ],
-  //         },
-  //       ],
-  //     });
-  //     const gallery = dbGalleryData.get({ plain: true });
-  //     res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   }
-  // }
 });
 
 // GET one painting
@@ -103,23 +75,6 @@ router.get('/painting/:id', middleware, async (req, res) => {
     console.log(err);
     res.status(500).json(err);
   }
-
-  // If the user is not logged in, redirect the user to the login page
-  // if (!req.session.loggedIn) {
-  //   res.redirect('/login');
-  // } else {
-  //   // If the user is logged in, allow them to view the painting
-  //   try {
-  //     const dbPaintingData = await Painting.findByPk(req.params.id);
-
-  //     const painting = dbPaintingData.get({ plain: true });
-
-  //     res.render('painting', { painting, loggedIn: req.session.loggedIn });
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   }
-  // }
 });
 
 router.get('/login', (req, res) => {
@@ -129,6 +84,15 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
 });
 
 module.exports = router;
