dd09e787c4c8f032770db348939c615d344bd5da
Steve Calla <callasteven@gmail.com>
2022-09-20 14:08:35 -0600

    Add createPost javascript and route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -283,4 +283,22 @@ router.get("/update-posts/", middleware, async (req, res) => {
   });
 });
 
+router.post('/create-post', async (req, res) => {
+
+  console.log('create post = ', req.body);
+
+  try {
+    const dbPostData = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.userId,
+    });
+
+    res.status(200).json(dbPostData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
